7591668913e0ef268e42437ca10b98854258f557
anntgc00492GitHub <anntgc00492@gmail.com>
2016-10-22 11:50:44 +0700

    ThemCacControllerViewTuSinhKhac

---
+++ QuanLySinhVien/QuanLySinhVien.Web/Areas/Admin/Controllers/AssignmentsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuanLySinhVien.Data;
+using QuanLySinhVien.Models.Models;
+
+namespace QuanLySinhVien.Web.Areas.Admin.Controllers
+{
+    public class AssignmentsController : Controller
+    {
+        private QuanLySinhVienDbContext db = new QuanLySinhVienDbContext();
+
+        // GET: Admin/Assignments
+        public ActionResult Index()
+        {
+            var assignments = db.Assignments.Include(a => a.Course).Include(a => a.Instructor);
+            return View(assignments.ToList());
+        }
+
+        // GET: Admin/Assignments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assignment assignment = db.Assignments.Find(id);
+            if (assignment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(assignment);
+        }
+
+        // GET: Admin/Assignments/Create
+        public ActionResult Create()
+        {
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title");
+            ViewBag.InstructorID = new SelectList(db.Instructors, "PersonID", "FirstName");
+            return View();
+        }
+
+        // POST: Admin/Assignments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "InstructorID,CourseID")] Assignment assignment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Assignments.Add(assignment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", assignment.CourseID);
+            ViewBag.InstructorID = new SelectList(db.Instructors, "PersonID", "FirstName", assignment.InstructorID);
+            return View(assignment);
+        }
+
+        // GET: Admin/Assignments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assignment assignment = db.Assignments.Find(id);
+            if (assignment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", assignment.CourseID);
+            ViewBag.InstructorID = new SelectList(db.Instructors, "PersonID", "FirstName", assignment.InstructorID);
+            return View(assignment);
+        }
+
+        // POST: Admin/Assignments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "InstructorID,CourseID")] Assignment assignment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(assignment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CourseID = new SelectList(db.Courses, "CourseID", "Title", assignment.CourseID);
+            ViewBag.InstructorID = new SelectList(db.Instructors, "PersonID", "FirstName", assignment.InstructorID);
+            return View(assignment);
+        }
+
+        // GET: Admin/Assignments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assignment assignment = db.Assignments.Find(id);
+            if (assignment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(assignment);
+        }
+
+        // POST: Admin/Assignments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Assignment assignment = db.Assignments.Find(id);
+            db.Assignments.Remove(assignment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ QuanLySinhVien/QuanLySinhVien.Web/Areas/Admin/Controllers/OfficeAssignmentsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuanLySinhVien.Data;
+using QuanLySinhVien.Models.Models;
+
+namespace QuanLySinhVien.Web.Areas.Admin.Controllers
+{
+    public class OfficeAssignmentsController : Controller
+    {
+        private QuanLySinhVienDbContext db = new QuanLySinhVienDbContext();
+
+        // GET: Admin/OfficeAssignments
+        public ActionResult Index()
+        {
+            var officeAssigments = db.OfficeAssigments.Include(o => o.Instructor);
+            return View(officeAssigments.ToList());
+        }
+
+        // GET: Admin/OfficeAssignments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OfficeAssignment officeAssignment = db.OfficeAssigments.Find(id);
+            if (officeAssignment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(officeAssignment);
+        }
+
+        // GET: Admin/OfficeAssignments/Create
+        public ActionResult Create()
+        {
+            ViewBag.InstructorID = new SelectList(db.Instructors, "PersonID", "FirstName");
+            return View();
+        }
+
+        // POST: Admin/OfficeAssignments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "InstructorID,Location")] OfficeAssignment officeAssignment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.OfficeAssigments.Add(officeAssignment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.InstructorID = new SelectList(db.Instructors, "PersonID", "FirstName", officeAssignment.InstructorID);
+            return View(officeAssignment);
+        }
+
+        // GET: Admin/OfficeAssignments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OfficeAssignment officeAssignment = db.OfficeAssigments.Find(id);
+            if (officeAssignment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.InstructorID = new SelectList(db.Instructors, "PersonID", "FirstName", officeAssignment.InstructorID);
+            return View(officeAssignment);
+        }
+
+        // POST: Admin/OfficeAssignments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "InstructorID,Location")] OfficeAssignment officeAssignment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(officeAssignment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.InstructorID = new SelectList(db.Instructors, "PersonID", "FirstName", officeAssignment.InstructorID);
+            return View(officeAssignment);
+        }
+
+        // GET: Admin/OfficeAssignments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OfficeAssignment officeAssignment = db.OfficeAssigments.Find(id);
+            if (officeAssignment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(officeAssignment);
+        }
+
+        // POST: Admin/OfficeAssignments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            OfficeAssignment officeAssignment = db.OfficeAssigments.Find(id);
+            db.OfficeAssigments.Remove(officeAssignment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
